b230f806bb49b9d8bc8b783396540fd64693e46d
Cathal Tummon <cathal.tummon@addvance-it.com>
2019-07-26 13:53:36 +0100

    iOS NFC NDEF support

--- ios/Classes/UniLinksPlugin.m
+++ ios/Classes/UniLinksPlugin.m
@@ -10,6 +10,7 @@ static NSString *const kEventsChannel = @"uni_links/events";
 
 @implementation UniLinksPlugin {
   FlutterEventSink _eventSink;
+  API_AVAILABLE(ios(11.0)) API_UNAVAILABLE(macos, watchos, tvos) NFCCallbacks* _nfcCallbacks;
 }
 
 static id _instance;
@@ -80,9 +81,40 @@ static id _instance;
     result(self.initialLink);
     // } else if ([@"getLatestLink" isEqualToString:call.method]) {
     //     result(self.latestLink);
-  } else {
+  } else if ([@"startNFCSession" isEqualToString:call.method]) {
+      if (@available(iOS 11, *)){
+          NSLog(@"Starting nfc session");
+          if(_nfcCallbacks == nil) {
+              _nfcCallbacks = [[NFCCallbacks alloc] init];
+              _nfcCallbacks.delegate = self;
+              if([call.arguments isKindOfClass:[NSString class]]){
+                  _nfcCallbacks.dialogMessage = call.arguments;
+              }
+          }
+          [_nfcCallbacks start];
+      }
+  } else if ([@"stopNFCSession" isEqualToString:call.method]) {
+      if (@available(iOS 11, *)){
+          NSLog(@"Stopping nfc session");
+          if(_nfcCallbacks != nil){
+              [_nfcCallbacks stop];
+          }
+          _nfcCallbacks = nil;
+      }
+  }
+  else {
     result(FlutterMethodNotImplemented);
   }
+}
+
+- (void) foundUri: (NSString* _Nullable) uri {
+    if(uri != nil){
+        NSLog(@"Found uri");
+        self.latestLink = uri;
+        if (!_eventSink) {
+            self.initialLink = self.latestLink;
+        }
+    }
 }
 
 - (FlutterError *_Nullable)onListenWithArguments:(id _Nullable)arguments
@@ -94,6 +126,72 @@ static id _instance;
 - (FlutterError *_Nullable)onCancelWithArguments:(id _Nullable)arguments {
   _eventSink = nil;
   return nil;
+}
+
+@end
+
+@implementation NFCCallbacks
+
+- (void) readerSession:(NFCNDEFReaderSession *)session didDetectNDEFs:(NSArray<NFCNDEFMessage *> *)messages {
+    NSLog(@"readerSession, detected ndef messages.");
+    if (@available(iOS 11, *)) {
+        if(session == _nfcReaderSession){
+            for(NFCNDEFMessage* message in messages) {
+                if(message == nil){
+                    continue;
+                }
+                for(NFCNDEFPayload* payload in message.records){
+                    if(payload == nil){
+                        continue;
+                    }
+                    if(payload.typeNameFormat == NFCTypeNameFormatAbsoluteURI){
+                        if (payload.type == nil){
+                            continue;
+                        }
+                        if([payload.type length] > 0){
+                            NSString* dataStr = [NSString stringWithUTF8String:[payload.type bytes]];
+                            if(dataStr != nil){
+                                NSLog(@"URI ok, send to delegate.");
+                                if ([self.delegate respondsToSelector:@selector(foundUri:)]) {
+                                    [self.delegate foundUri:dataStr];
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+- (void)readerSession:(NFCNDEFReaderSession *)session didInvalidateWithError:(NSError *)error {
+    if(session == _nfcReaderSession){
+        _nfcReaderSession = nil;
+    }
+    if(error != nil){
+        NSLog(@"readerSession error: %@", error);
+    }
+}
+
+- (void) start {
+    if (@available(iOS 11, *)) {
+        if(NFCNDEFReaderSession.readingAvailable){
+            if (_nfcReaderSession == nil ){
+                _nfcReaderSession = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:nil invalidateAfterFirstRead:YES];
+                _nfcReaderSession.alertMessage = _dialogMessage;
+            }
+            [_nfcReaderSession beginSession];
+        } else {
+            NSLog(@"NFCReaderSession is not available");
+        }
+    }
+}
+
+- (void) stop {
+    if (_nfcReaderSession != nil) {
+        [_nfcReaderSession invalidateSession];
+        _nfcReaderSession = nil;
+    }
 }
 
 @end
