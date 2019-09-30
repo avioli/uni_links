4fc23d46d80f1b036120c3452bbfbc43c86a651a
Sebastian Roth <sebastian.roth@gmail.com>
2019-09-30 14:56:36 +0100

    Add NS_ASSUME_NONNULL* macro to reduce compiler warnings.

--- ios/Classes/UniLinksPlugin.h
+++ ios/Classes/UniLinksPlugin.h
@@ -1,8 +1,12 @@
 #import <Flutter/Flutter.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface UniLinksPlugin : NSObject <FlutterPlugin>
 + (instancetype)sharedInstance;
 - (BOOL)application:(UIApplication *)application
     continueUserActivity:(NSUserActivity *)userActivity
       restorationHandler:(void (^)(NSArray *_Nullable))restorationHandler;
 @end
+
+NS_ASSUME_NONNULL_END
