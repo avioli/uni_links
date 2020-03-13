fee5088ae7d987f363366473d2cc16932b1b608b
若月豊 <yutaka.wakatsuki@topgate.co.jp>
2020-03-13 13:48:36 +0900

    fix: sceneを追加,各イベントにログを追加

--- ios/Classes/UniLinksPlugin.m
+++ ios/Classes/UniLinksPlugin.m
@@ -52,6 +52,7 @@ static id _instance;
   NSURL *url = (NSURL *)launchOptions[UIApplicationLaunchOptionsURLKey];
   self.initialLink = [url absoluteString];
   self.latestLink = self.initialLink;
+    NSLog(@"didFinishLaunchingWithOptionsを呼んだ:%@", url);
   return YES;
 }
 
@@ -59,7 +60,14 @@ static id _instance;
             openURL:(NSURL *)url
             options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
   self.latestLink = [url absoluteString];
+    NSLog(@"openURLを読んだ self.latestLink: %@", self.latestLink);
   return YES;
+}
+
+- (void)scene:(UIScene *)scene openURLContexts:(nonnull NSSet<UIOpenURLContext *> *)URLContexts
+API_AVAILABLE(ios(13.0)){
+    NSURL *url = [[URLContexts allObjects] firstObject].URL;
+ NSLog(@"scene:(UIScene *)scene openURLContextsを呼んだ url: %@", url);
 }
 
 - (BOOL)application:(UIApplication *)application
@@ -70,12 +78,14 @@ static id _instance;
     if (!_eventSink) {
       self.initialLink = self.latestLink;
     }
+      NSLog(@"continueUserActivityを呼んだ initialLink: %@", self.initialLink);
     return YES;
   }
   return NO;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
+    NSLog(@"handleMethodCall call.method:%@",call.method);
   if ([@"getInitialLink" isEqualToString:call.method]) {
     result(self.initialLink);
     // } else if ([@"getLatestLink" isEqualToString:call.method]) {
@@ -88,11 +98,13 @@ static id _instance;
 - (FlutterError *_Nullable)onListenWithArguments:(id _Nullable)arguments
                                        eventSink:(nonnull FlutterEventSink)eventSink {
   _eventSink = eventSink;
+    NSLog(@"onListenWithArguments");
   return nil;
 }
 
 - (FlutterError *_Nullable)onCancelWithArguments:(id _Nullable)arguments {
   _eventSink = nil;
+    NSLog(@"onCancelWithArguments");
   return nil;
 }
 
