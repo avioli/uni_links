3c4822ec08aadd7facee4fe0330b63a43333dd26
hasanmahani08 <hasanmahani08@gmail.com>
2023-05-21 13:26:52 +0330

    dart fix added

--- uni_links/example/lib/main.dart
+++ uni_links/example/lib/main.dart
@@ -196,7 +196,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
               '(tap on any of the above commands to print it to'
               ' the console/logger and copy to the device clipboard.)',
               textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.caption,
+              style: Theme.of(context).textTheme.bodySmall,
             ),
           ]
         ].expand((el) => el).toList(),
@@ -222,7 +222,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   }
 
   void _showSnackBar(String msg) {
-    WidgetsBinding.instance?.addPostFrameCallback((_) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
       final context = _scaffoldKey.currentContext;
       if (context != null) {
         ScaffoldMessenger.of(context).showSnackBar(SnackBar(
